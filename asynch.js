f21bc0d53f164491f7dc05f1d591d46e57421a3d
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-03-05 21:12:49 -0600

    complete, changed to asynch

--- asynch.js
+++ asynch.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
-const {readFileSync} = require('fs');
+const {readFile, readFileSync} = require('fs');
 
 const input = process.argv[2];
-
 const output = readFileSync(input);
 
-process.stdout.write(output);
+readFile(input, (err, data) => {
+    if (err) throw err;
+    process.stdout.write(output);
+
+})
